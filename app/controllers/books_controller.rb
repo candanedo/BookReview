f15c8baac3fa73794782e244dc1ad70a8a3cc7a4
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2019-03-24 04:03:24 -0600

    Implement images for books to represent the cover
    We want to be able to attrach a file for now to each book that we create
    that way we can use this file as the cover image of the book.
    
    For this we will use Active Storage which actually comes as default storage
    for file attachement. For now this is the configuration only for local
    development environment.
    
    This implement active storage for image attachment in books only for
    development environment.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -48,7 +48,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :description, :author, :user_id, :category_id)
+    params.require(:book).permit(:title, :description, :author, :user_id, :category_id, :cover_image)
   end
 
   def find_book
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_one_attached :cover_image
 
   scope :by_category, ->(category_id) { where(category_id: category_id) }
   scope :order_by_title, ->() { order('title') }
